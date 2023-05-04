2678c043d14c76ce3c1e842186f529e973203dbb
kishorek10 <131877759+kishorek10@users.noreply.github.com>
2023-05-04 22:34:44 +0530

    added basic setup terraform code (#6)
    added basic setup terraform code

---
+++ workshops/l100/confluent_on_azure_basics/Confluent_basic_setup/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.39.0"
+    }
+  }
+}
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+resource "confluent_environment" "staging" {
+  display_name = "Confluent_Training"
+}
+
+# Stream Governance and Kafka clusters can be in different regions as well as different cloud providers,
+# but you should to place both in the same cloud and region to restrict the fault isolation boundary.
+data "confluent_schema_registry_region" "essentials" {
+  cloud   = "AZURE"
+  region  = "westus2"
+  package = "ESSENTIALS"
+}
+
+resource "confluent_schema_registry_cluster" "essentials" {
+  package = data.confluent_schema_registry_region.essentials.package
+
+  environment {
+    id = confluent_environment.staging.id
+  }
+
+  region {
+    # See https://docs.confluent.io/cloud/current/stream-governance/packages.html#stream-governance-regions
+    id = data.confluent_schema_registry_region.essentials.id
+  }
+}
+# Update the config to use a cloud provider and region of your choice.
+# https://registry.terraform.io/providers/confluentinc/confluent/latest/docs/resources/confluent_kafka_cluster
+resource "confluent_kafka_cluster" "standard" {
+  display_name = "C1"
+  availability = "SINGLE_ZONE"
+  cloud        = "AZURE"
+  region       = "eastus2"
+  standard {}
+  environment {
+    id = confluent_environment.staging.id
+  }
+}
